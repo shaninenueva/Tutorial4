bec79e230da2c66cd82c164213429586c850cf8e
Shanine <nuevashanine@gmail.com>
2020-08-25 00:31:03 +1200

    Added test for Subtract method

--- src/test/java/tutorial4/TestCalc.java
+++ src/test/java/tutorial4/TestCalc.java
@@ -2,17 +2,27 @@ package tutorial4;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestCalc {
+	@BeforeEach
+	void setUp() throws Exception {
+	}
 
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+	
 	@Test
 	void testAdd() {
 		assertEquals(55, new Calc().Add(20, 35));
+	}
+	
+	@Test
+	void testSubtract() {
+		assertEquals(15, new Calc().Subtract(20, 5));
 	}
 
 }
